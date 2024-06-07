eac82da8e863f681d0eee1c0435a81fa293f9314
Salman Hossen <hossainsalman873@gmail.com>
2024-06-07 14:47:37 +0600

    2nd Code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='text-5xl text-center p-3 bg-indigo-400 text-white'> hello world</h1>
+    </>
+  )
+}
+
+export default App;
